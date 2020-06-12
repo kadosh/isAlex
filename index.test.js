c5da8145911f8b7ad5118c42c652493424230abb
Alejandro Ocampo <alejandro.ocampo@audatex.com>
2020-06-11 19:30:58 -0500

    s

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const isAlex = require("./index");
+
+describe("isAlex", () => {
+    it("should be alex", () => {
+        expect(isAlex("Alex")).toBeTruthy();
+    });
+
+    it("should not be alex", () => {
+        expect(isAlex("Aalex")).toBe(false);
+    });
+});
